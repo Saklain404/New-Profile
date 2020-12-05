df609291a88c1b5acc9ef12902dc43a48c181bdb
saklain404 <mohdsaklain8899@gmail.com>
2020-12-06 00:22:00 +0530

    Updates

---
+++ precache-manifest.3bb0a8854d35b2c4118d5a8aebffce36.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14919410b9d7e1a494004541db7e8d5f",
+    "url": "/Saklain404/New-Profile/index.html"
+  },
+  {
+    "revision": "a689b129c30d522ffafc",
+    "url": "/Saklain404/New-Profile/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "2dca239fad42a3d3a72f",
+    "url": "/Saklain404/New-Profile/static/css/main.61a5340d.chunk.css"
+  },
+  {
+    "revision": "a689b129c30d522ffafc",
+    "url": "/Saklain404/New-Profile/static/js/2.e4d73e5f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Saklain404/New-Profile/static/js/2.e4d73e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dca239fad42a3d3a72f",
+    "url": "/Saklain404/New-Profile/static/js/main.b52efa74.chunk.js"
+  },
+  {
+    "revision": "94f9bce7dfefc8167cfa",
+    "url": "/Saklain404/New-Profile/static/js/runtime-main.4666eb89.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/Saklain404/New-Profile/static/media/AntDesign.3a2ba315.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/Saklain404/New-Profile/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/Saklain404/New-Profile/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/Saklain404/New-Profile/static/media/Feather.6beba7e6.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Saklain404/New-Profile/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/Saklain404/New-Profile/static/media/Foundation.e20945d7.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/Saklain404/New-Profile/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "d15c1216957060fac577af6151fb8cfe",
+    "url": "/Saklain404/New-Profile/static/media/MaterialCommunityIcons.d15c1216.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/Saklain404/New-Profile/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "73b8cff012825060b308d2162f31dbb2",
+    "url": "/Saklain404/New-Profile/static/media/Octicons.73b8cff0.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/Saklain404/New-Profile/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "5cdf883b18a5651a29a4d1ef276d2457",
+    "url": "/Saklain404/New-Profile/static/media/Zocial.5cdf883b.ttf"
+  },
+  {
+    "revision": "33c2ecb06d27dc8262114d1568edc63c",
+    "url": "/Saklain404/New-Profile/static/media/photo.33c2ecb0.jpeg"
+  }
+]);
